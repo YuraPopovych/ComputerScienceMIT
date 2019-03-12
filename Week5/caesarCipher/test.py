9f17f2ba4488691b01049af9e6f8d43f859d2eca
YuraPopovych <c.ypopovych@gogoair.com>
2019-03-13 00:25:57 +0200

    add test case for PlainTextMessage instance

--- Week5/caesarCipher/test.py
+++ Week5/caesarCipher/test.py
@@ -1,10 +1,12 @@
 from ps6 import *
 
-text = "Hello, my friend!"
-myMessage = Message(text)
-print(myMessage.build_shift_dict(0))
-print(myMessage.apply_shift(2))
+# text = "Hello, my friend!"
+# myMessage = Message(text)
+# print(myMessage.build_shift_dict(0))
+# print(myMessage.apply_shift(2))
 
-# plaintext = PlaintextMessage('hello', 2)
-# print('Expected Output: jgnnq')
-# print('Actual Output:', plaintext.get_message_text_encrypted())
+plaintext = PlaintextMessage('hello', 2)
+print('Expected Output: jgnnq')
+print('Actual Output:', plaintext.get_message_text_encrypted())
+plaintext.change_shift(26)
+print('Actual Output2:', plaintext.get_message_text_encrypted())
