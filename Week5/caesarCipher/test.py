89cc16241520c0c575c6ca1c89a5ca809d9bb7cf
YuraPopovych <c.ypopovych@gogoair.com>
2019-03-12 23:21:56 +0200

    implement built shift method

---
+++ Week5/caesarCipher/test.py
@@ -0,0 +1,10 @@
+from ps6 import *
+
+text = "Hello, my friend!"
+myMessage = Message(text)
+print(myMessage.build_shift_dict(0))
+print(myMessage.apply_shift(2))
+
+# plaintext = PlaintextMessage('hello', 2)
+# print('Expected Output: jgnnq')
+# print('Actual Output:', plaintext.get_message_text_encrypted())
